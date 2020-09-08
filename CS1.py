25784e7a71599b7bee6f6760e597e28c24e42067
Squeeshy123 <62818641+Squeeshy123@users.noreply.github.com>
2020-09-08 11:22:29 -0400

    Added comments

--- CS1.py
+++ CS1.py
@@ -8,6 +8,7 @@ def chaos():
         print(x) # output
 # 3.9 * 0.15 * (1 - 0.15)
 
+# make a sandwich function
 def pbJ():
     print("make a sandwich")
     main_food = input("what is your main food item? ")
@@ -20,6 +21,7 @@ def pbJ():
     if(done == "n"):
         print("well too bad we can't buy any more groceries and now we're out of " + second_food)
 
+# makes a basic window using pygame (not part of the chapter)
 def window():
     (width, height) = (300, 200)
     screen = pygame.display.set_mode((width, height))
@@ -29,20 +31,20 @@ def window():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                #da
 
+# Celsuis to Fahrenheit calculator
 def Faran():
     print("Hey this is a program to convert the normal temprature to american temprature")
     c = int(input("What is the current temp? (C): "))
     f =  9 / 5  * c + 32
     print(str(c) + " is " + str(f) + " in Fahrenheit")
 
-
+# Average score Calculator
 def Scorese():
     scores = list()
-    scores_amount = input("Enter how many elements you want:")
-    print ("Enter numbers in array: ")
-    for i in range(int(scores_amount)):
+    scores_amount = input("Enter how many elements you want:") # Ask how many scores there are
+    print ("Enter numbers in array: ") # ask what the scores are
+    for i in range(int(scores_amount)): # does the calculation for each score
         n = input("Score: ")
         scores.append(int(n))
     current_score = 0
@@ -51,25 +53,26 @@ def Scorese():
             current_score = scores[i] + scores[i - 1] / len(scores)
         else:
             print("not enough data, yet.....")
-    print(current_score)
-def test():
+    print(current_score) # prints score once calculations are done
+def test(): # just a test function, Ignore.
     print("start")
     for i in range(0): 
         print("hello")
     print("end")
 
-def brr():
+def brr(): # You can't multiply strings! Watch me.
     i = 0
     print("python go " + "b"+ "r" * 100)
-def iteration():
+def iteration(): # prints binary numbers up to 3273390607896141870013189696827599152216642046043064789483291368096133796404674554883270092325904157150886684127560071009217256545885393053328527589376
     c = 1
+    print(1)
     for i in range(500):
-        
         c = c + c
         print(c)
 
-def main():
-    Faran()
 
-if __name__=="__main__": 
+def main(): 
+    Selector() # change this function to whatever 
+
+if __name__=="__main__": # runs the main function
     main() 
